e4269ce765443e1103e00148b0b720aef31f4d8c
MechaFinch <33042726+MechaFinch@users.noreply.github.com>
2019-11-03 14:58:40 -0500

    Continued work on GUI design

--- src/mechafinch/sim/GUI/E8SimulatorExecutable.java
+++ src/mechafinch/sim/GUI/E8SimulatorExecutable.java
@@ -16,6 +16,8 @@ import java.awt.Font;
 import java.awt.Insets;
 import java.awt.CardLayout;
 import javax.swing.JTabbedPane;
+import javax.swing.border.BevelBorder;
+import javax.swing.JTextPane;
 
 public class E8SimulatorExecutable {
 
@@ -72,6 +74,7 @@ public class E8SimulatorExecutable {
 		frame.getContentPane().add(registersPanel, gbc_registersPanel);
 		
 		JTabbedPane ramRomTabs = new JTabbedPane(JTabbedPane.TOP);
+		ramRomTabs.setBorder(null);
 		GridBagConstraints gbc_ramRomTabs = new GridBagConstraints();
 		gbc_ramRomTabs.insets = new Insets(0, 0, 5, 0);
 		gbc_ramRomTabs.fill = GridBagConstraints.BOTH;
@@ -79,38 +82,23 @@ public class E8SimulatorExecutable {
 		gbc_ramRomTabs.gridy = 1;
 		frame.getContentPane().add(ramRomTabs, gbc_ramRomTabs);
 		
-		JPanel ramPanel = new JPanel();
-		ramRomTabs.addTab("RAM", null, ramPanel, null);
-		ramPanel.setLayout(new BoxLayout(ramPanel, BoxLayout.Y_AXIS));
+		JScrollPane ramPane = new JScrollPane();
+		ramPane.setViewportBorder(null);
+		ramRomTabs.addTab("RAM", null, ramPane, null);
 		
-		JLabel lblRam = new JLabel("RAM");
-		lblRam.setHorizontalAlignment(SwingConstants.CENTER);
-		ramPanel.add(lblRam);
+		JTextPane ramText = new JTextPane();
+		ramText.setFont(new Font("Courier New", Font.PLAIN, 14));
+		ramText.setText("     00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F\r\n\r\n00   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n10   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n20   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n30   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n40   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n50   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n60   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n70   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n80   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n90   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\nA0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\nB0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\nC0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\nD0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\nE0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\nF0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00");
+		ramPane.setViewportView(ramText);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		ramPanel.add(scrollPane);
+		JScrollPane romPane = new JScrollPane();
+		romPane.setViewportBorder(null);
+		ramRomTabs.addTab("ROM", null, romPane, null);
 		
-		JTextArea ramTextArea = new JTextArea();
-		ramTextArea.setFont(new Font("Courier New", Font.PLAIN, 14));
-		ramTextArea.setEditable(false);
-		ramTextArea.setText("lmao");
-		scrollPane.setViewportView(ramTextArea);
-		
-		JPanel romPanel = new JPanel();
-		ramRomTabs.addTab("ROM", null, romPanel, null);
-		romPanel.setLayout(new BoxLayout(romPanel, BoxLayout.Y_AXIS));
-		
-		JLabel lblRom = new JLabel("ROM");
-		lblRom.setHorizontalAlignment(SwingConstants.CENTER);
-		romPanel.add(lblRom);
-		
-		JScrollPane scrollPane_1 = new JScrollPane();
-		romPanel.add(scrollPane_1);
-		
-		JTextArea romTextArea = new JTextArea();
-		romTextArea.setEditable(false);
-		romTextArea.setText("oaml");
-		scrollPane_1.setViewportView(romTextArea);
+		JTextPane romText = new JTextPane();
+		romText.setFont(new Font("Courier New", Font.PLAIN, 14));
+		romText.setText("      00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F\r\n\r\n000   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n010   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n020   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n030   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n040   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n050   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n060   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n070   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n080   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n090   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n0A0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n0B0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n0C0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n0D0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n0E0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n0F0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n100   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n110   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n120   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n130   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n140   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n150   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n160   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n170   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n180   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n190   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n1A0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n1B0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n1C0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n1D0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n1E0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n1F0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n200   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n210   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n220   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n230   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n240   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n250   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n260   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n270   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n280   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n290   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n2A0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n2B0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n2C0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n2D0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n2E0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n2F0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n300   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n310   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n320   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n330   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n340   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n350   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n360   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n370   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n380   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n390   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n3A0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n3B0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n3C0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n3D0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n3E0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\r\n3F0   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00");
+		romPane.setViewportView(romText);
 	}
 
 }
